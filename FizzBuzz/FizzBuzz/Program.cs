4b95b27210bd5f87d889496ce9b28f256af15986
Colton Miller <ColtonJMMiller@gmail.com>
2015-04-08 14:27:56 -0600

    Finished with code

--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -10,9 +10,32 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+            for (int i = 0; i <= 20; i++)
+            {
+                Console.WriteLine(FizzBuzz(i));   
+            }
+            Console.ReadKey();
         }
         public static string FizzBuzz(int number)
         {
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                return "fizzbuzz";
+            }
+            else if (number % 5 == 0)
+            {
+                return "buzz";
+            }
+            else if (number % 3 == 0)
+            {
+                return "fizz";
+            }
+            else
+            {
+                string numString = number.ToString();
+                return numString;
+            }
+
 
         }   
     }
